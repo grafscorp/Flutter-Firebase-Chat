803a7cfd89be7208e94b5f53ac99458feeb22574
Grafscorp <golyandringraf@gmail.com>
2025-01-03 01:25:47 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/pages/login_page.dart';
+import 'package:flutter_chat/themes/flutter_midnight_neon_theme.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: FluttterMidnightNeonTheme.lightTheme,
+      darkTheme: FluttterMidnightNeonTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
