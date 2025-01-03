619e0318500801e533fa0edaa663329b699a7634
Grafscorp <golyandringraf@gmail.com>
2025-01-03 22:18:26 +0300

    Create post_edit.dart

---
+++ lib/components/posts/post_edit.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/services/posts_database.dart';
+
+class PostEdit extends StatefulWidget {
+  const PostEdit({super.key});
+
+  @override
+  State<PostEdit> createState() => _PostEditState();
+}
+
+class _PostEditState extends State<PostEdit> {
+  TextEditingController postController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 40,
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(20),
+            ),
+            color: Theme.of(context).colorScheme.onError),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: () async {
+                    if (postController.text.isEmpty) return;
+                    PostsDatabase postsDatabase = PostsDatabase();
+                    await postsDatabase.addPost(postController.text);
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.send,
+                    size: 60,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: TextField(
+                controller: postController,
+                maxLines: 50,
+                decoration: InputDecoration(
+                    hintText: "Text post...", border: OutlineInputBorder()),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
