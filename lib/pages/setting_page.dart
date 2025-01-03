f249a81e65b7292c53675aae9a65e77a48e2b520
Grafscorp <golyandringraf@gmail.com>
2025-01-04 02:21:57 +0300

    fix layouts

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/pages/auth_page.dart';
+import 'package:flutter_chat/pages/login_page.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -11,14 +13,21 @@ class SettingPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Settings"),
       ),
-      body: Column(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ElevatedButton(
-              onPressed: () async {
-                FirebaseAuth.instance.signOut();
-                Navigator.pop(context);
-              },
-              child: Text("Log Out"))
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                  onPressed: () async {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.popUntil(context, ModalRoute.withName('/'));
+                  },
+                  child: Text("Log Out"))
+            ],
+          ),
         ],
       ),
     );
