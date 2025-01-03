627abe4ac81260b90f9792624dea5dae5e4722eb
Grafscorp <golyandringraf@gmail.com>
2025-01-03 22:18:30 +0300

    Update user_list_tile_circle.dart

--- lib/components/user_list_tile_circle.dart
+++ lib/components/user_list_tile_circle.dart
@@ -1,40 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/models/user_chat.dart';
+import 'package:flutter_chat/pages/account_page.dart';
 
 class UserListTileCircle extends StatelessWidget {
-  UserListTileCircle({super.key, required this.userName, this.userAvatar});
+  UserListTileCircle({super.key, required this.user});
 
-  String userName;
-  String? userAvatar;
+  UserChat user;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 150,
       width: 150,
-      child: Container(
-        padding: EdgeInsets.all(20),
-        margin: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-            shape: BoxShape.circle, color: Theme.of(context).highlightColor),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Spacer(),
-            CircleAvatar(
-              radius: 50,
-              foregroundImage: AssetImage("assets/images/template_avatar.png"),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              userName,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
+      child: InkWell(
+        radius: 150,
+        child: Container(
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              shape: BoxShape.circle, color: Theme.of(context).highlightColor),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Spacer(),
+              CircleAvatar(
+                radius: 50,
+                foregroundImage:
+                    AssetImage("assets/images/template_avatar.png"),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                user.username != null ? user.username! : "Error",
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
         ),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return AccountPage(user: user);
+              },
+            ),
+          );
+        },
       ),
     );
   }
