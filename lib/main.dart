91006ecef8010c6ec505bc42466e4e5440a64cf7
Grafscorp <golyandringraf@gmail.com>
2025-01-03 07:19:23 +0300

    Debug main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/pages/home_page.dart';
 import 'package:flutter_chat/pages/login_page.dart';
+import 'package:flutter_chat/pages/message_user_page.dart';
 import 'package:flutter_chat/themes/flutter_midnight_neon_theme.dart';
 
 void main() {
@@ -15,7 +17,7 @@ class MainApp extends StatelessWidget {
       theme: FluttterMidnightNeonTheme.lightTheme,
       darkTheme: FluttterMidnightNeonTheme.darkTheme,
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: MessageUserPage(),
     );
   }
 }
