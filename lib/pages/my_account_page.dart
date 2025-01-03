b9441a2936efd286a5893eb9ba717ba3012ad45f
Grafscorp <golyandringraf@gmail.com>
2025-01-04 02:21:29 +0300

    update state connection

--- lib/pages/my_account_page.dart
+++ lib/pages/my_account_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/components/info_dialogs.dart';
 import 'package:flutter_chat/components/userPage/profile_appbar.dart';
 import 'package:flutter_chat/models/user_chat.dart';
 import 'package:flutter_chat/pages/account_page.dart';
@@ -13,11 +14,13 @@ class MyAccountPage extends StatelessWidget {
       future: UserChat.parseUserFirebase(FirebaseAuth.instance.currentUser!),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
+          //showErrorDialog(context, "", des)
           FirebaseAuth.instance.signOut();
           return Text("Error");
         } else if (snapshot.hasData) {
           return AccountPage(user: snapshot.data!);
-        }
+        } else if (snapshot.connectionState == ConnectionState.waiting)
+          return CircularProgressIndicator();
         return CircularProgressIndicator();
       },
     );
