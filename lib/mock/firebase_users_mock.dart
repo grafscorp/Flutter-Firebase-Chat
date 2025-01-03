301cbc9c34ff7118f281c840323150a63b1151e5
Grafscorp <golyandringraf@gmail.com>
2025-01-03 07:23:06 +0300

    Test mockdata

---
+++ lib/mock/firebase_users_mock.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_chat/components/user_list_tile_circle.dart';
+
+List<UserListTileCircle> users_list = [
+  UserListTileCircle(userName: "Meida"),
+  UserListTileCircle(userName: "Hecker"),
+  UserListTileCircle(userName: "SiroChan"),
+  UserListTileCircle(userName: "Vla"),
+  UserListTileCircle(userName: "SiroChasdasdasdasdan"),
+];
